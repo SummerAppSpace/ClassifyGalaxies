23dd2e0391237b266a7e07d175e3bf713d699fed
iksworm <iksworm@outlook.com>
2017-08-04 17:36:16 +0000

    Image sharpening with function

--- image_sharpening.py
+++ image_sharpening.py
@@ -1,20 +1,25 @@
 from PIL import ImageFilter
 from PIL import Image, ImageChops
 
-#Read image
-im = Image.open('100042.jpg')   #'100037.jpg' '100018.jpg'
-#Display image
-#Applying a filter to the image
-im_sharp = im.filter( ImageFilter.SHARPEN )
-#Saving the filtered image to a new file
-im_sharp.save( 'image_sharpened3.jpg', 'JPEG' )
 
-#Splitting the image into its respective bands, i.e. Red, Green,
-#and Blue for RGB
-r,g,b = im_sharp.split()
+def image_sharpening(Image):
+    ''' image is taken as a parameter to input all images to code'''
 
-#Viewing EXIF data embedded in image
-exif_data = im._getexif()
-exif_data
-im.save('fixedimg3.png')
-residual = ImageChops.subtract(im,im)
+    #Read image
+    im = Image.open('100042.jpg')   #'100037.jpg' '100018.jpg'
+    #Display image
+    #Applying a filter to the image
+    im_sharp = im.filter( ImageFilter.SHARPEN )
+    #Saving the filtered image to a new file
+    im_sharp.save( 'image_sharpened3.jpg', 'JPEG' )
+
+    #Splitting the image into its respective bands, i.e. Red, Green,
+    #and Blue for RGB
+    r,g,b = im_sharp.split()
+
+    residual = ImageChops.subtract(im,im)
+
+    #Viewing EXIF data embedded in image
+    exif_data = im._getexif()
+    exif_data
+    im.save('fixedimg4.png')
