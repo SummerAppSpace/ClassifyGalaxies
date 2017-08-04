7f693f2b3584aa5827f883bca6ef7995c8f42712
123tshi <123tshi@gmail.com>
2017-08-04 19:57:34 +0000

    added comments, made code more efficient

--- image_sharpening.py
+++ image_sharpening.py
@@ -1,25 +1,24 @@
 from PIL import ImageFilter
-from PIL import Image, ImageChops
+from PIL import Image
 
 
-def image_sharpening(Image):
+def image_sharpening(image):
     ''' image is taken as a parameter to input all images to code'''
 
     #Read image
-    im = Image.open('100042.jpg')   #'100037.jpg' '100018.jpg'
+    im = Image.open(image)   #'100037.jpg' '100018.jpg'
     #Display image
     #Applying a filter to the image
-    im_sharp = im.filter( ImageFilter.SHARPEN )
+    im_sharp = im.filter(ImageFilter.SHARPEN)
     #Saving the filtered image to a new file
-    im_sharp.save( 'image_sharpened3.jpg', 'JPEG' )
+    im_sharp.save('tmp_image_sharpened.jpg', 'JPEG')
 
     #Splitting the image into its respective bands, i.e. Red, Green,
     #and Blue for RGB
     r,g,b = im_sharp.split()
 
-    residual = ImageChops.subtract(im,im)
-
     #Viewing EXIF data embedded in image
     exif_data = im._getexif()
     exif_data
-    im.save('fixedimg4.png')
+    newimage = im
+    return newimage
